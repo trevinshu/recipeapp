acbc6bcae6f25e252bc1f00a561b07af0b0fe67c
trevinshu <trevinshu2008@gmail.com>
2022-05-27 21:47:33 -0600

    added favicon

--- dist/script.d573be0b.js
+++ dist/script.d573be0b.js
@@ -1143,7 +1143,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55559" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50291" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
