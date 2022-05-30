aa08d456527b52931f8b41532cda0fd03adc5aee
trevinshu <trevinshu2008@gmail.com>
2022-05-29 21:05:43 -0600

    reduced margin

--- dist/script.d573be0b.js
+++ dist/script.d573be0b.js
@@ -1143,7 +1143,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52669" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57162" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
